ea83bc87b02745320c511b3b99698dd60b64649b
Kevin Lannon <klannon@nd.edu>
2021-12-26 16:55:39 -0600

    Implementation of PolynomialLayer and PolySplineLayer

--- kpl_layers/polynomial.py
+++ kpl_layers/polynomial.py
@@ -25,7 +25,14 @@ class MonomialLayer(nn.Module):
        n_outputs: The number of monomials output
 
     """
-    def __init__(self, n_inputs: int, degree: int):
+    def __init__(self, n_inputs: int, degree: int) -> None:
+        """Constructor
+
+        Args:
+            n_inputs: The number of input variables to the layer
+            degree: The maximum degree of the monomials
+        """
+        
         super(MonomialLayer,self).__init__()
 
         self.n_inputs = n_inputs
@@ -56,8 +63,128 @@ class MonomialLayer(nn.Module):
             self.m_ind[row,:] =  curr_ind
 
     def forward(self, x:torch.Tensor) -> torch.Tensor:
+        """Performs forward propagation for this module
+
+        Args: 
+            x: 
+              The input tensor to this layer.  This function preserves
+              all indices except the last one, which is assumed to
+              index the input variables.  Leading indices can be used
+              for minibatches or structuring the inputs.
+
+        """
+        
         if x.shape[-1] != self.n_inputs:
             raise IndexError(f'Expecting {self.n_inputs} inputs, got {x.shape[-1]}')
         x = torch.cat((torch.ones(x.shape[:-1]+(1,)),x),axis=-1)
         return torch.prod(torch.index_select(x,-1,self.m_ind.flatten())
                           .reshape(x.shape[:-1]+self.m_ind.shape),axis=-1)
+
+
+class PolynomialLayer(nn.Module):
+    """Calculates one or more polynomials of the inputs.
+
+    For a given list of inputs (as few as one) and a given degree,
+    calculate one or more polynomials.  The polynomial coefficients
+    are trainable weights.
+
+    Attributes:
+       n_inputs: The number of inputs this layer expects
+       degree: The maximum degree of the polynomial
+       n_outputs: The number of polynomials output
+
+    """
+    def __init__(self, n_inputs: int,
+                 degree: int,
+                 n_outputs:int) -> None:
+        """Constructor
+
+        Args:
+            n_inputs: The number of input variables to the layer
+            degree: The maximum degree of the polynomials
+        """
+        
+        super(PolynomialLayer,self).__init__()
+
+        self.n_inputs = n_inputs
+        self.degree = degree
+        self.n_outputs = n_outputs
+
+        self.monomial_layer = MonomialLayer(self.n_inputs,self.degree)
+        self.linear = nn.Linear(self.monomial_layer.n_outputs,self.n_outputs)
+        
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        """Performs forward propagation for this module
+
+        Args: 
+            x: 
+              The input tensor to this layer.  This function preserves
+              all indices except the last one, which is assumed to
+              index the input variables.  Leading indices can be used
+              for minibatches or structuring the inputs.
+
+        """
+        
+        if x.shape[-1] != self.n_inputs:
+            raise IndexError(f'Expecting {self.n_inputs} inputs, got {x.shape[-1]}')
+        return self.linear(self.monomial_layer(x))
+
+class PolySplineLayer(nn.Module):
+    """Approximates a function using a combination of multiple polynomials
+    controlled by a softmax attention function.
+
+    Attributes:
+       n_inputs: The number of inputs this layer expects
+
+    """
+    def __init__(self, n_inputs: int,
+                 degree: int,
+                 n_attn: int,
+                 n_outputs:int) -> None:
+        """Constructor
+
+        Args:
+            n_inputs: The number of input variables to the layer
+            degree: The maximum degree of the polynomials
+        """
+        
+        super(PolySplineLayer,self).__init__()
+
+        self.n_inputs = n_inputs
+        self.degree = degree
+        self.n_attn = n_attn
+        self.n_outputs = n_outputs
+
+        self.monomial_layer = MonomialLayer(self.n_inputs,self.degree)
+        self.poly_coeffs = nn.ModuleList()
+        self.attention = nn.ModuleList()
+        for i in range(n_outputs):
+            self.poly_coeffs.append(nn.Linear(self.monomial_layer.n_outputs,self.n_attn))
+            self.attention.append(nn.Sequential(
+                nn.Linear(self.n_inputs,10*self.n_attn),
+                nn.ReLU(),
+                nn.Linear(10*self.n_attn,self.n_attn),
+                nn.Softmax(dim=-1),
+            ))
+                                  
+            
+        
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        """Performs forward propagation for this module
+
+        Args: 
+            x: 
+              The input tensor to this layer.  This function preserves
+              all indices except the last one, which is assumed to
+              index the input variables.  Leading indices can be used
+              for minibatches or structuring the inputs.
+
+        """
+        
+        if x.shape[-1] != self.n_inputs:
+            raise IndexError(f'Expecting {self.n_inputs} inputs, got {x.shape[-1]}')
+
+        out = [torch.sum(poly(self.monomial_layer(x))*attn(x),dim=-1,keepdim=True)
+               for poly,attn in zip(self.poly_coeffs,self.attention)]
+
+        return torch.cat(out)
